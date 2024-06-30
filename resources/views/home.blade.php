66d0453be592b384573374d2135b55abea5a15b1
KrisnaMurti-21 <krisnamr27@gmail.com>
2024-06-30 17:42:45 +0700

    second commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Pegawai</title>
+    <link href="/bootstrap/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body class="bg-secondary">
+    @livewire('employee')
+    <script src="/bootstrap/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
